7ee9e11167690563bfc37e100f44af626d4d9dcd
Andrew Ho <andrew.ho@stitchfix.com>
2018-07-01 11:46:15 -0700

    Add a BiLSTM option to model (not sure if this will work yet)

--- word_language_model/model.py
+++ word_language_model/model.py
@@ -3,20 +3,41 @@ import torch.nn as nn
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False):
+    def __init__(self,
+                 rnn_type,
+                 ntoken,
+                 ninp,
+                 nhid,
+                 nlayers,
+                 dropout=0.5,
+                 tie_weights=False):
         super(RNNModel, self).__init__()
         self.drop = nn.Dropout(dropout)
         self.encoder = nn.Embedding(ntoken, ninp)
-        if rnn_type in ['LSTM', 'GRU']:
-            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout=dropout)
+        if rnn_type == 'BiLSTM':
+            self.rnn = nn.LSTM(ninp,
+                               nhid,
+                               nlayers,
+                               dropout=dropout,
+                               bidirectional=True)
+            self.decoder = nn.Linear(2*nhid, ntoken)
+        elif rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, rnn_type)(ninp,
+                                             nhid,
+                                             nlayers,
+                                             dropout=dropout)
+            self.decoder = nn.Linear(nhid, ntoken)
         else:
             try:
-                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+                nonlinearity = {'RNN_TANH': 'tanh',
+                                'RNN_RELU': 'relu'}[rnn_type]
             except KeyError:
-                raise ValueError( """An invalid option for `--model` was supplied,
-                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+                raise ValueError(
+                    "An invalid option for `--model` was supplied, "
+                    "options are ['BiLSTM', 'LSTM', 'GRU', 'RNN_TANH' or "
+                    "'RNN_RELU']")
             self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
-        self.decoder = nn.Linear(nhid, ntoken)
+            self.decoder = nn.Linear(nhid, ntoken)
 
         # Optionally tie weights as in:
         # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
@@ -25,9 +46,12 @@ class RNNModel(nn.Module):
         # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
         # https://arxiv.org/abs/1611.01462
         if tie_weights:
-            if nhid != ninp:
-                raise ValueError('When using the tied flag, nhid must be equal to emsize')
-            self.decoder.weight = self.encoder.weight
+            if rnn_type == 'BiLSTM':
+                print("Warning, BiLSTM can not have tied weights!")
+            else:
+                if nhid != ninp:
+                    raise ValueError('When using the tied flag, nhid must be equal to emsize')
+                self.decoder.weight = self.encoder.weight
 
         self.init_weights()
 
@@ -50,7 +74,10 @@ class RNNModel(nn.Module):
 
     def init_hidden(self, bsz):
         weight = next(self.parameters())
-        if self.rnn_type == 'LSTM':
+        if self.rnn_type == 'BiLSTM':
+            return (weight.new_zeros(self.nlayers*2, bsz, self.nhid),
+                    weight.new_zeros(self.nlayers*2, bsz, self.nhid))
+        elif self.rnn_type == 'LSTM':
             return (weight.new_zeros(self.nlayers, bsz, self.nhid),
                     weight.new_zeros(self.nlayers, bsz, self.nhid))
         else:
